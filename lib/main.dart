3307da4b7490285b3d8c7255f519e0d6356b62eb
dev-KarimAhmed <dev-KarimAhmed@gmail.com>
2023-08-05 00:43:30 +0300

    change icon and name of application

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'package:sleep_calculator/views/bedtime_view.dart';
 import 'package:sleep_calculator/views/home_page.dart';
 
